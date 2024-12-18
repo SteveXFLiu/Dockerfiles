c949af9d73d0afbd67ccce8f540ef81f5fcf5a73
steveleo <stevexfliu@gmail.com>
2024-12-18 03:51:15 -0500

    Shell script to install Python

---
+++ scripts/install_python.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e -x
+
+# a function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+# Install dependencies
+apt_install \
+    curl \
+    software-properties-common
+
+# Add PPA
+add-apt-repository ppa:deadsnakes/ppa
+
+# Install Python
+apt_install python${PYTHON_VERSION:-3.9}
+
+# Set default Python version
+ln -sf /usr/bin/python${PYTHON_VERSION:-3.9} /usr/bin/python3
+
+# Install and upgrade pip
+apt_install python3.9-distutils
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+python3 -m pip install --upgrade pip
+
+# Install Python packages
+python3 -m pip install --no-cache-dir -r /init_scripts/requirements.txt
+
+# Clean up
+rm get-pip.py
+rm -rf /var/lib/apt/lists/*
